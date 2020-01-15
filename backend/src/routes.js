5581d3a324512199a6ac47e1ae2b101a8226ba55
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2020-01-14 22:42:46 -0300

    New index and store functions for the controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,26 +1,9 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev')
 const routes = Router();
+const DevController = require('./controllers/DevController')
 
-routes.post('/devs', async (request, response)=>{
-    const { github_username, techs} = request.body;
+routes.get('/devs', DevController.index); 
+routes.post('/devs', DevController.store); 
 
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
-    
-    const { name = login, avatar_url, bio } = apiResponse.data;
-
-    const techsArray = techs.split(',').map(tech => tech.trim());
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-    })
-
-    return response.json(dev)
-}); 
 
 module.exports = routes
