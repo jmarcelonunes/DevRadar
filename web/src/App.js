2490bbe0e5861fe96023185c8d703b0b0ff30a4e
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2020-01-16 00:13:02 -0300

    Complete Interface with API connection

--- web/src/App.js
+++ web/src/App.js
@@ -1,81 +1,41 @@
-import React, { useState } from 'react';
+import React, {useState ,useEffect } from 'react';
+import api from './services/api';
+import DevItem from './components/DevItem'
+import DevForm from './components/DevForm'
 import './global.css'
 import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
 function App() {
+  const[devs, setDevs] = useState([]);
+
+  useEffect(() =>{
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data)
+    }
+    loadDevs();
+  }, [])
+
+  async function handleAddDev(data){
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-          
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div> 
-          
-          <div className="input-group">
-          
-          <div className="input-block">
-            <label htmlFor="Latidude">Latidude</label>
-            <input name="Latidude" id="Latidude" required/>
-          </div> 
-          
-          <div className="input-block">
-            <label htmlFor="techs">Longitude</label>
-            <input name="Longitude" id="Longitude" required/>
-          </div> 
-
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
-
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/31075972?s=460&v=4" alt="Joao Marcelo"/>
-              <div className="user-info">
-                <strong>Joao Marcelo</strong>
-                <span>React, C, C++</span>
-              </div>
-            </header>
-            <p>Computer Engineering Student at University of Brasília</p>
-            <a href="https://github.com/jmarcelonunes">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/31075972?s=460&v=4" alt="Joao Marcelo"/>
-              <div className="user-info">
-                <strong>Joao Marcelo</strong>
-                <span>React, C, C++</span>
-              </div>
-            </header>
-            <p>Computer Engineering Student at University of Brasília</p>
-            <a href="https://github.com/jmarcelonunes">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/31075972?s=460&v=4" alt="Joao Marcelo"/>
-              <div className="user-info">
-                <strong>Joao Marcelo</strong>
-                <span>React, C, C++</span>
-              </div>
-            </header>
-            <p>Computer Engineering Student at University of Brasília</p>
-            <a href="https://github.com/jmarcelonunes">Acessar perfil no Github</a>
-          </li>
-          
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
       </main>
     </div>
